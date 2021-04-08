a0401a88d91f8c2f4e94283da93c56a9ca04f386
Ehsan Faraji-Jalal <ehsan@internetnz.net.nz>
2021-04-08 16:35:12 +1200

    Check origin header to be allowed

--- inz_irma_backend/backend.py
+++ inz_irma_backend/backend.py
@@ -1,10 +1,12 @@
 """
 The implementation of InternetNZ IRMA backend
 """
+import os
 import traceback
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_login import LoginManager, login_required, UserMixin
 from werkzeug.exceptions import HTTPException
 
 from inz_irma_backend import single_source, logger
@@ -12,11 +14,17 @@ from inz_irma_backend import single_source, logger
 app = Flask(__name__)
 CORS(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+ALLOWED_ORIGIN = os.environ.get('ALLOWED_ORIGIN')
+
 
 class InternalServerError(Exception):
     """
     Internal server error class.
     """
+
     def __init__(self, description=None, code=None):
         Exception.__init__(self)
         self.description = description if description else \
@@ -25,6 +33,7 @@ class InternalServerError(Exception):
         self.name = "Internal Server Error"
 
 
+@app.errorhandler(401)
 @app.errorhandler(InternalServerError)
 @app.errorhandler(HTTPException)
 def error_handler(error):
@@ -37,10 +46,11 @@ def error_handler(error):
         "name": error.name,
         "description": error.description,
     })
-    return response
+    return response, error.code
 
 
 @app.route('/single-source/drivers-licences', methods=['POST'])
+@login_required
 def verify_driver_license():
     """
     A wrapper on /drivers-licences SingleSource API.
@@ -60,6 +70,7 @@ def verify_driver_license():
 
 
 @app.route('/single-source/passports', methods=['POST'])
+@login_required
 def verify_passport():
     """
     A wrapper on /passports SingleSource API.
@@ -79,6 +90,7 @@ def verify_passport():
 
 
 @app.route('/single-source/doughnuts/<doughnut>')
+@login_required
 def verify_doughnut(doughnut):
     """
     A wrapper on /doughnuts SingleSource API.
@@ -92,3 +104,29 @@ def verify_doughnut(doughnut):
         logger.error('Internal error: %s - %s', str(exc),
                      traceback.format_exc())
         raise InternalServerError() from exc
+
+
+@login_manager.request_loader
+def check_origin(api_request):
+    """
+    Checks origin header to be matched by ALLOWED_ORIGIN.
+    """
+    logger.debug('Authorize api call by checking api call origin')
+
+    origin = api_request.headers.get('Origin')
+
+    logger.debug('Origin: %s', origin)
+
+    if origin != ALLOWED_ORIGIN:
+        return None
+
+    return ApiUser(origin)
+
+
+class ApiUser(UserMixin):
+    """
+    Flask login user class
+    """
+
+    def __init__(self, origin):
+        self.id = origin  # pylint: disable=invalid-name
